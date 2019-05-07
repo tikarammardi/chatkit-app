58694cb11229d4c13b8d11d7b9957cf98b702a8a
Tikaram Mardi <tikarammardi007@gmail.com>
2019-05-07 15:06:48 +0530

    bugs bugs bugs

---
+++ src/config.js
@@ -0,0 +1,5 @@
+const tokenUrl =
+  'https://us1.pusherplatform.io/services/chatkit_token_provider/v1/f7d1bdb6-5361-403a-906f-b203fd0a05b4/token';
+const instanceLocator = 'v1:us1:f7d1bdb6-5361-403a-906f-b203fd0a05b4';
+
+export { tokenUrl, instanceLocator };
