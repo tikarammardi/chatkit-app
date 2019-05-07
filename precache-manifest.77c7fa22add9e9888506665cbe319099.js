616551970ef4955595455f501d9bad65753f09f8
Tikaram Mardi <tikarammardi007@gmail.com>
2019-05-07 15:27:57 +0530

    Updates

---
+++ precache-manifest.77c7fa22add9e9888506665cbe319099.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27903983721cb8d7df5322899e9c46f2",
+    "url": "/tikarammardi/chatkit-app/index.html"
+  },
+  {
+    "revision": "3106abb7103ef392ddad",
+    "url": "/tikarammardi/chatkit-app/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "c217c5eee319399bebc7",
+    "url": "/tikarammardi/chatkit-app/static/js/2.2f55c4dd.chunk.js"
+  },
+  {
+    "revision": "3106abb7103ef392ddad",
+    "url": "/tikarammardi/chatkit-app/static/js/main.9ec92db7.chunk.js"
+  },
+  {
+    "revision": "7d912d903e841c7890ff",
+    "url": "/tikarammardi/chatkit-app/static/js/runtime~main.dff1424b.js"
+  }
+]);
